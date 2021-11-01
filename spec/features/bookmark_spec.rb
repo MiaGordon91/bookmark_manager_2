35721f14e27124751c8c53f0dba15fa8d6fa6625
Rebecca <Beckyrose200@aol.com>
2021-11-01 16:02:17 +0000

    Setting up database

--- spec/features/bookmark_spec.rb
+++ spec/features/bookmark_spec.rb
@@ -1,10 +1,4 @@
 
-
-#  Write a failing feature test for viewing bookmarks at the /bookmarks route.
-#  Pass the feature test in the simplest way possible, by hard coding some bookmarks, for now.
-#  Refactor the code to use the View and Controller.
-#  Test drive a refactor of the code to use a Model, that returns the list of bookmarks.
-
 feature 'bookmark' do
   scenario 'viewing bookmarks' do
     visit '/bookmarks'
