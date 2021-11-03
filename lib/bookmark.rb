4d9151558a2a6b060079b689e9f70af45783604d
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2021-11-03 16:50:20 +0000

    refactored fviewing bookmark feature test to factor in create method

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -11,14 +11,16 @@ class Bookmark
     result = connection.exec("SELECT * FROM bookmarks")
     result.map { |bookmark| bookmark['url'] }
   end
+
+  def self.create(url:)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_manager')
+    end
+  
+    connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+
+  end 
 end
 
-
-
-# @bookmarks=['Bookmark 1', 'Bookmark 2' ,'Bookmark 3']
-  
-#   def self.all
-#       @bookmarks
-#   end
-
-# end
